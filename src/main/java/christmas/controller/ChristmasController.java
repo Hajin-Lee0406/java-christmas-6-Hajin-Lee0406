530a7f985d6fb46ea7e9d5308176e6936099b4d3
Hajin-Lee0406 <glgkwls1@naver.com>
2023-11-14 23:48:29 +0900

    feat: 방문 날짜 입력 view, controller 연결

--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -1,12 +1,23 @@
 package christmas.controller;
 
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
 public class ChristmasController {
+    private final OutputView outputView;
+    private final InputView inputView;
+
+    public ChristmasController() {
+        outputView = OutputView.getInstance();
+        inputView = InputView.getInstance();
+    }
+
     public void run() {
         start();
     }
 
     private void start(){
-
+        int visitDate = inputView.readDate();
     }
 
 }
