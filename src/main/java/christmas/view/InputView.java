b94d94183c90d83014e58fb034cd8bf90cb77a04
Hajin-Lee0406 <glgkwls1@naver.com>
2023-11-14 23:54:27 +0900

    feat: 방문 날짜 입력 예외 처리 코드 추가

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -14,6 +14,18 @@ public class InputView {
 
     public int readDate() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        return Integer.parseInt(Console.readLine());
+        int date;
+
+        try {
+            date = Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException("[ERROR] 1 ~ 31 사이의 날짜를 입력해주세요");
+        }
+
+        if (date < 1 || date > 31) {
+            throw new IllegalArgumentException("[ERROR] 1 ~ 31 사이의 날짜를 입력해주세요");
+        }
+
+        return date;
     }
 }
