dacb587be3e66de48c601ade4643c147ef2f1536
Hajin-Lee0406 <glgkwls1@naver.com>
2023-11-14 23:43:01 +0900

    feat: 예상 방문 날짜 입력

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,5 +1,7 @@
 package christmas.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
     private static final InputView instance = new InputView();
 
@@ -8,5 +10,10 @@ public class InputView {
     }
 
     private InputView() {
+    }
+
+    public int readDate() {
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        return Integer.parseInt(Console.readLine());
     }
 }
